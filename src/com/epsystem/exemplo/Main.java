4e5bce3666014639536140f3bf38de8b0448f560
Elson Pinheiro <elson.silva@ufopa.edu.br>
2021-11-24 11:47:48 -0300

    add arquivos

--- src/com/epsystem/exemplo/Main.java
+++ src/com/epsystem/exemplo/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Cliente joao = new Cliente("João", "joao@xyz.com", "34999-4455");
-		Cliente maria = new Cliente("maria", "maria@xut.com", "934444-2233");
+		Cliente maria = new Cliente("Maria", "maria@xut.com", "934444-2233");
 		
 		AtivacaoClienteService ativacaoCliente = new AtivacaoClienteService();
 		ativacaoCliente.Ativar(joao);
---
+++ src/com/epsystem/exemplo/service/EmissaoNotaFiscalService.java
@@ -0,0 +1,5 @@
+package com.epsystem.exemplo.service;
+
+public class EmissaoNotaFiscalService {
+
+}
